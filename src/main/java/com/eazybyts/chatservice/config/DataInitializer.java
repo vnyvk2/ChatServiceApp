07f678e7dbdee55580bad26be8cf3b985dfa6a04
Vinay Vennela <vny.vk2@gmail.com>
2025-11-18 16:46:19 +0530

    Add application.properties and database initialization

---
+++ src/main/java/com/eazybyts/chatservice/config/DataInitializer.java
@@ -0,0 +1,33 @@
+package com.eazybyts.chatservice.config;
+
+import com.eazybyts.chatservice.model.ERole;
+import com.eazybyts.chatservice.model.Role;
+import com.eazybyts.chatservice.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Initialize roles if they don't exist
+        if (roleRepository.findByName(ERole.ROLE_USER).isEmpty()) {
+            roleRepository.save(new Role(ERole.ROLE_USER));
+        }
+
+        if (roleRepository.findByName(ERole.ROLE_MODERATOR).isEmpty()) {
+            roleRepository.save(new Role(ERole.ROLE_MODERATOR));
+        }
+
+        if (roleRepository.findByName(ERole.ROLE_ADMIN).isEmpty()) {
+            roleRepository.save(new Role(ERole.ROLE_ADMIN));
+        }
+
+        System.out.println("Database initialization completed!");
+    }
+}
